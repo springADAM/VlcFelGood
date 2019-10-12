e4cacebf80e3091fefd8be87a1c0c20cc082477c
BendabizAdam <bendabiza.adam@gmail.com>
2019-10-12 23:30:18 +0100

    added fullscreen feature

--- src/myPack/Controller.java
+++ src/myPack/Controller.java
@@ -1,8 +1,12 @@
 package myPack;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.MediaView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import java.io.File;
@@ -27,6 +31,19 @@ public class Controller {
             m = new MPlayer(file,pContainer);
         }catch (NullPointerException | MalformedURLException e){
             m = new MPlayer(file,pContainer);
+        }
+    }
+    @FXML
+    AnchorPane FullAnchor = new AnchorPane();
+    public void screen(MouseEvent e){
+        if(e.getClickCount() == 2) {
+            MediaView fullm = m.getMv();
+            fullm.setPreserveRatio(false);
+            fullm.fitWidthProperty().bind(FullAnchor.widthProperty());
+            fullm.fitHeightProperty().bind(FullAnchor.heightProperty());
+            FullAnchor.getChildren().add(fullm);
+            Main.pstage.setScene(new Scene(FullAnchor));
+            Main.pstage.setFullScreen(true);
         }
     }
 
