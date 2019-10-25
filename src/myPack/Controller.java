7c699dbbe80e9d0f88c5abd2567d0ea6896831f1
rania-bensakina <43249131+rania-bensakina@users.noreply.github.com>
2019-10-25 19:11:59 +0100

    added close and enhassed features

--- src/myPack/Controller.java
+++ src/myPack/Controller.java
@@ -1,21 +1,23 @@
 package myPack;
 
+import javafx.application.Application;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 import javafx.beans.binding.Bindings;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.Slider;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.media.MediaView;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -23,30 +25,33 @@ import javafx.util.Duration;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
 import java.util.ResourceBundle;
 
-public class Controller  {
+import static myPack.Main.pstage;
+
+public class Controller extends Application {
 
     @FXML
     public MenuItem open = new MenuItem();
     public Button pause = new Button();
-
     private static File file;
     private MPlayer m;
     private boolean isfullscreen = false;
-    private boolean playerstate =false;
+    private boolean playerstate =true;
+    public MenuItem close = new MenuItem();
 
-    @FXML  Label startTime = new Label();
-    @FXML Label endTime = new Label();
-
-  @FXML  MenuItem speed = new MenuItem();
+    @FXML public Menu openRecently = new Menu();
+    @FXML  Label endTime = new Label();
+    @FXML  Label playTime= new Label();
+    @FXML  MenuItem speed = new MenuItem();
 
     @FXML
     public Slider time = new Slider();
     public Slider vol = new Slider();
     @FXML
     AnchorPane pContainer = new AnchorPane();
-    java.time.Duration strtTime;
+
 
 
     public void openFile() throws MalformedURLException {
@@ -54,14 +59,25 @@ public class Controller  {
             FileChooser fileChooser = new FileChooser();
             fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Video files (*.mp4)", "*.mp4"), new FileChooser.ExtensionFilter("Audio files (*.mp3)", "*.mp3"));
             file = fileChooser.showOpenDialog(new Stage());
+            MenuItem item = new MenuItem();
+            item.setText(file.getPath());
+            item.setOnAction(event -> {
             m.getMediaPlayer().stop();
-            m = new MPlayer(file, pContainer,time);
+            try {
+                m = new MPlayer(file, pContainer, time, playTime);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            Main.pstage.setTitle(file.getName());
+            });
+            openRecently.getItems().add(item);
+            m.getMediaPlayer().stop();
+            m = new MPlayer(file, pContainer,time,playTime);
             slide();
         } catch (NullPointerException | MalformedURLException e) {
-            m = new MPlayer(file, pContainer,time);
+            m = new MPlayer(file, pContainer,time,playTime);
         }
     }
-
     @FXML
     AnchorPane FullAnchor = new AnchorPane();
     Scene temp = new Scene(FullAnchor);
@@ -81,8 +97,8 @@ public class Controller  {
                     isfullscreen=false;
                 }
             });
-            Main.pstage.setScene(temp);
-            Main.pstage.setFullScreen(true);
+            pstage.setScene(temp);
+            pstage.setFullScreen(true);
             isfullscreen = true;
             event.consume();
         } else {
@@ -95,19 +111,18 @@ public class Controller  {
     }
 
 
-
     public void backtonormal() {
         m.getMv().fitWidthProperty().bind(pContainer.widthProperty());
         m.getMv().fitHeightProperty().bind(pContainer.heightProperty());
         pContainer.getChildren().removeAll();
         pContainer.getChildren().add(m.getMv());
-        Main.pstage.setScene(Main.pscene);
+        pstage.setScene(Main.pscene);
     }
 
     @FXML
     public void pausePlay(){
         try{
-            if(playerstate) {m.getMediaPlayer().pause(); playerstate =false;}
+            if(playerstate) {m.getMediaPlayer().pause(); playerstate =false; }
             else { m.getMediaPlayer().play(); playerstate = true;}}catch (NullPointerException e){}
     }
 
@@ -117,22 +132,30 @@ public class Controller  {
     @FXML public void fster () {
         m.getMediaPlayer().setRate(1.5);
     }
-
-
-
-
+    @FXML public void slw () {
+        m.getMediaPlayer().setRate(0.75);
+    }
+    @FXML public void slwer () {
+        m.getMediaPlayer().setRate(0.5);
+    }
 
 
     public void slide() {
-
           time.valueProperty().addListener(ov -> {
            if (time.isPressed()) {
              m.getMediaPlayer().seek(m.getMediaPlayer().getMedia().getDuration().multiply(time.getValue() / 100));
+             /*  time.valueChangingProperty().addListener(new ChangeListener<Boolean>() {
+                   @Override
+                   public void changed(ObservableValue<? extends Boolean> source, Boolean oldValue, Boolean newValue) {
+                       playTime.textProperty().setValue(String.valueOf((int)time.getValue()));
+                   } });*/
        }
 
-
-
     });}
+    public void close(){
+        System.exit(0);
+    }
+
     @FXML
     public void changevol(){
         {
@@ -145,4 +168,10 @@ public class Controller  {
             });
 
         }}
+
+
+    @Override
+    public void start(Stage pStage) throws Exception {
+
+    }
 }
--- src/myPack/MPlayer.java
+++ src/myPack/MPlayer.java
@@ -3,18 +3,28 @@ package myPack;
 import javafx.application.Platform;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
+import javafx.beans.binding.Bindings;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.util.Duration;
+
 import java.io.File;
 import java.net.MalformedURLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+
 
 class MPlayer {
     private  MediaPlayer mediaPlayer ;
     private  MediaView mv;
-    MPlayer(File file, AnchorPane pContainer,Slider time) throws MalformedURLException {try{
+    DateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+    MPlayer(File file, AnchorPane pContainer, Slider time, Label playTime) throws MalformedURLException {try{
         Media media = new Media(file.toURI().toURL().toExternalForm());
         mediaPlayer = new MediaPlayer(media);
         mv = new MediaView(mediaPlayer);
@@ -24,19 +34,30 @@ class MPlayer {
         pContainer.getChildren().removeAll();
         pContainer.getChildren().add(mv);
         mediaPlayer.setAutoPlay(true);}catch(NullPointerException ignored){}
-        mediaPlayer.currentTimeProperty().addListener(ov -> updatesValues(time));
+
+        mediaPlayer.currentTimeProperty().addListener(ov -> {
+
+
+
+            playTime.textProperty().bind(Bindings.createStringBinding(() -> String.valueOf(mediaPlayer.getCurrentTime().toMillis())));
+            
+            updatesValues(time);});
         time.valueProperty().addListener(ov -> {
             if (time.isPressed()) {
                 mediaPlayer.seek(mediaPlayer.getMedia().getDuration().multiply(time.getValue() / 100));
             }
+
         });
+
     }
 
-    protected void updatesValues(Slider time)
+    private void updatesValues(Slider time)
     {
         Platform.runLater(() -> time.setValue(mediaPlayer.getCurrentTime().toMillis()/
                 mediaPlayer.getTotalDuration().toMillis()* 100));
+
     }
+
 
     public MediaView getMv() {
         return mv;
